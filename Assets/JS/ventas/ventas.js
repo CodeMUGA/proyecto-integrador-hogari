a404872897620068bfe4d02e22f66fc4ac35cb83
Sandra Ruiz <sandraa_so@hotmail.es>
2022-06-14 16:38:59 -0600

    ÂCorreccion de ortografia

--- Assets/JS/ventas/ventas.js
+++ Assets/JS/ventas/ventas.js
@@ -18,7 +18,7 @@ function addItems(div_Productos) { //div_Productos es el div donde se va a agreg
                         <img class="bd-placeholder-img card-img-top" role="img" src="./Assets/img/${p.imagen}" />
                         <div class="card-body">
                         <p class="card-text"><strong>${p.direccion}</strong></p>
-                        <p class="card-text">${p.tipoInmueble}</p>
+                        <p class="card-text">${p.descripcion}</p>
                         <div class="d-flex justify-content-between align-items-center">
                             
                             <small class="text-muted">$ ${p.precio} MXN</small>
@@ -50,3 +50,19 @@ function view(index) { //index es el índice del producto que se va a ver
     <strong>$ ${productos[index].price} MXN<strong>`; //se cambia el cuerpo del modal
     $("#productModal").modal("show"); //se muestra el modal
 }// view
+
+
+fetch(URL_MAIN, { //URL del servicio a donde se hara el POST
+    method: 'POST', // or 'PUT' 
+    headers: { // se agrega el header
+      'Content-Type': 'application/json', //tipo de contenido
+    },
+    body: JSON.stringify(data), //se agrega el cuerpo del POST
+  })
+  .then(response => response.json()) //se obtiene la respuesta del servidor
+  .then(data => { //se obtiene el json
+    console.log('Success:', data); //se imprime el json
+  })
+  .catch((error) => { //si hay un error
+    console.error('Error:', error); //se imprime el error
+  });
